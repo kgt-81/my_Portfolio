68ae982d8b589ab5c9dab589fdb661e3d2ecab26
kgt-81 <97212951+kgt-81@users.noreply.github.com>
2022-02-25 14:32:41 +0900

    修正

--- js/script.js
+++ js/script.js
@@ -1,18 +1,28 @@
 $(function () {
 
   //ページ内スクロール
-  var navHeight = $(".header").outerHeight();
+  let navHeight = $(".header").outerHeight();
+
+  let topBtn = $("#js-page-top");
 
   $('a[href^="#"]').on("click", function () {
-    var href = $(this).attr("href");
-    var target = $(href == "#" || href == "" ? "html" : href);
-    var position = target.offset().top - navHeight;
+    let href = $(this).attr("href");
+    let target = $(href == "#" || href == "" ? "html" : href);
+    let position = target.offset().top - navHeight;
     $("html, body").animate({ scrollTop: position, }, 300, "swing");
     return false;
   });
 
+  $(window).on('scroll load',function scroll_effect() {
+    if($(this).scrollTop()>120){
+      topBtn.fadeIn();
+    }else{
+      topBtn.fadeOut();
+    }
+  });
+
   //ページトップ
-  $("#js-page-top").on("click", function () {
+  topBtn.on("click", function () {
     $("body,html").animate({ scrollTop: 0, }, 300);
     return false;
   });
